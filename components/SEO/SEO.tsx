f4853383144acd23842d143f4be27b4529c86c9e
cyishere <cyishere@gmail.com>
2021-12-16 17:59:18 +0800

    Add site image

--- components/SEO/SEO.tsx
+++ components/SEO/SEO.tsx
@@ -14,7 +14,7 @@ const SEO: React.FC<Props> = ({ title, altTitle, description, pathname }) => {
 
   const descripitonContent = description
     ? description
-    : "Thoughts on Frontend stuff - JavaScript, React, CSS, GraphQL and Prisma.";
+    : "Frontend Developer: React, Gatsby, JavaScript, TypeScript, CSS-in-Js, Storybook, TDD & a tiny bit of Apollo/GraphQL/Prisma";
   const canonical = pathname !== "/" ? `${domainUrl}${pathname}` : domainUrl;
 
   return (
@@ -25,6 +25,11 @@ const SEO: React.FC<Props> = ({ title, altTitle, description, pathname }) => {
       <meta name="robots" content="index, follow" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="canonical" href={canonical} />
+      <meta
+        name="image"
+        content={`${domainUrl}/images/cyishere-site-image.jpg`}
+      />
+      <meta name="image:alt" content={descripitonContent} />
     </Head>
   );
 };
