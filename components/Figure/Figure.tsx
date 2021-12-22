bd6e9fda2c1494f527a71f69dce3fd15d7bbfe4d
cyishere <cyishere@gmail.com>
2021-12-22 17:01:40 +0800

    Update Figure structure

--- components/Figure/Figure.tsx
+++ components/Figure/Figure.tsx
@@ -11,21 +11,37 @@ interface FigureProps {
 const Figure: React.FC<FigureProps> = ({ src, alt, width, height }) => {
   return (
     <Wrapper>
-      <Image src={src} alt={alt} layout="fixed" width={width} height={height} />
+      <ImgWrapper>
+        <Image
+          src={src}
+          alt={alt}
+          layout="responsive"
+          width={width}
+          height={height}
+        />
+      </ImgWrapper>
       <Figcaption>{alt}</Figcaption>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.figure`
-  text-align: center;
+  /* width: min(50vw, 100%); */
+  margin-left: -64px;
+  margin-right: -64px;
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+
+const ImgWrapper = styled.div`
+  width: 100%;
+  display: block;
   position: relative;
 `;
 
 const Figcaption = styled.figcaption`
+  text-align: center;
   color: var(--clr-text-light);
   font-size: var(--font-size-sm);
   margin-top: 10px;
