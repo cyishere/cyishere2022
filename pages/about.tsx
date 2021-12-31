b90103a9c3a6d936c8a5f9d74dbfff789668bc2c
cyishere <cyishere@gmail.com>
2021-12-31 10:06:46 +0800

    Add about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,15 +1,68 @@
+import styled from "styled-components";
+
 import { Layout } from "@/components/Layout";
 import { MaxWidthWrapper } from "@/components/MaxWidthWrapper";
 import SectionTitle from "@/components/SectionTitle";
+import Post from "@/components/Post";
+import Emoji from "@/components/Emoji";
+import { ExternalLink } from "@/components/TextLink";
 
-const about = () => {
-  return (
-    <Layout title="About CY">
-      <MaxWidthWrapper>
-        <SectionTitle variant="salmon">About Me</SectionTitle>
-      </MaxWidthWrapper>
-    </Layout>
-  );
-};
+const About = () => (
+  <Layout title="About CY">
+    <Wrapper as="main">
+      <SectionTitle variant="salmon" position="center">
+        About Me <Emoji name="Woman Technologist">👩🏻‍💻</Emoji>
+      </SectionTitle>
+      <Content>
+        <p>
+          <Emoji name="vulcan salute">🖖🏻</Emoji> Hello, my name is CY.
+        </p>
+        <p>
+          I am a self-taught <em>front-end developer</em> who also makes{" "}
+          <em>full-stack</em> stuff.
+        </p>
+        <p>
+          The first time I encountered “web development” was about 15 years ago
+          when I changed my blogpost blog’s background color with CSS, that’s
+          amazed me a lot!{" "}
+          <strong>I thought it’s very cool, the internet is the future!</strong>{" "}
+          Around that time, I just graduated from college, and despite I studied
+          finance I decided to change my career to web development. So I started
+          to learn programming by myself, reading books, articles, and
+          discussions online.
+        </p>
+        <p>
+          I got my first development job in 2007. From 2007 to 2012, I was a
+          frontend developer and UX designer, most of my work was writing CSS
+          hacks for IE6, and playing around with jQuery. Made a lot of templates
+          for WordPress and e-commerce websites.
+        </p>
+        <p>
+          Later I switched my career path and started a production company with
+          my friends. I was a screenplay writer in the last four years (
+          <strong>also self-taught</strong>). But I still love web development a
+          lot. Developers work on code and take films as entertainment, I worked
+          on films and take programming as entertainment. When I didn’t study
+          films, I read articles about web development and follow along with the
+          tutorials on{" "}
+          <ExternalLink href="https://scotch.io">🥃 scotch.io</ExternalLink>.
+        </p>
+        <p>
+          I spend last year catching up on the new tech stacks. Now, I&apos;m a
+          fan of React, GraphQL and Prisma.
+        </p>
+      </Content>
+    </Wrapper>
+  </Layout>
+);
 
-export default about;
+const Wrapper = styled(MaxWidthWrapper)`
+  background-color: var(--clr-white);
+  padding: 2rem 2rem 10rem 2rem;
+`;
+
+const Content = styled(Post)`
+  margin-top: 4rem;
+`;
+
+export default About;
