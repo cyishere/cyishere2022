d6087d1496d3ea01bfd532cf0c086450947b26db
cyishere <cyishere@gmail.com>
2022-01-05 20:22:34 +0800

    Add head info

--- components/SEO/SEO.tsx
+++ components/SEO/SEO.tsx
@@ -25,11 +25,36 @@ const SEO: React.FC<Props> = ({ title, altTitle, description, pathname }) => {
       <meta name="robots" content="index, follow" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="canonical" href={canonical} />
-      <meta
-        name="image"
-        content={`${domainUrl}/images/cyishere-site-image.jpg`}
-      />
+      <meta name="image" content={`${domainUrl}/images/og-default.png`} />
       <meta name="image:alt" content={descripitonContent} />
+      <meta property="og:title" content="Chen Yang, aka CY" />
+      <meta
+        property="og:description"
+        content="Frontend Developer: React, Gatsby, JavaScript, TypeScript, CSS-in-Js, Storybook, TDD & a tiny bit of Apollo/GraphQL/Prisma"
+      />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:image"
+        content={`${domainUrl}/images/og-default.png`}
+      />
+      <meta
+        property="og:image:alt"
+        content="Banner for cyishere.dev, featuring many tech stack icons"
+      />
+      <meta property="og:image:width" content="1280" />
+      <meta property="og:image:height" content="675" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content="@cyishere" />
+      <meta name="twitter:title" content="Chen Yang, aka CY" />
+      <meta
+        name="twitter:description"
+        content="Articles about front-end and some full-stack development. Focus on React, CSS, and more!"
+      />
+      <meta
+        name="twitter:image"
+        content={`${domainUrl}/images/og-default.png`}
+      />
+      <meta name="theme-color" content="hsl(246deg 62.5% 96.9%)" />
     </Head>
   );
 };
