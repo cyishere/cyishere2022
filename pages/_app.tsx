72bd16edc5d04ab0baa2ff3ecd08ed61ed3cc2cb
cyishere <cyishere@gmail.com>
2022-05-02 17:00:24 +0800

    Use React Fragment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,16 @@
 import type { AppProps } from "next/app";
+import React from "React";
 
 import GlobalStyles from "styles/GlobalStyles";
 import PrismMaterialLight from "@/styles/PrismMaterialLight";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <React.Fragment>
       <GlobalStyles />
       <Component {...pageProps} />
       <PrismMaterialLight />
-    </>
+    </React.Fragment>
   );
 }
 export default MyApp;
